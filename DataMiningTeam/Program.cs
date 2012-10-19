bde34a2a8f639b677ade7b6571b17abdd9abc566
amanda fouts <esantra03@gmail.com>
2012-10-18 21:45:50 -0400

    renamed form

--- DataMiningTeam/Program.cs
+++ DataMiningTeam/Program.cs
@@ -16,7 +16,7 @@ namespace DataMiningTeam
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new DMForm());
         }
     }
 }
