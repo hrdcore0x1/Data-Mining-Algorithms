d8491c437c34eae18f87f360f74d1ba93c4565dc
amanda fouts <esantra03@gmail.com>
2012-10-22 01:28:21 -0400

    trying to complete copied apriori algorithm connection using conn to db for testing purposes. not complete

---
+++ DataMiningTeam/IndexedDictionary.cs
@@ -0,0 +1,13 @@
+﻿//under solution file
+using System.Collections.ObjectModel;
+
+namespace AprioriAlgorithm
+{
+    public class IndexedDictionary : KeyedCollection<string, Item>
+    {
+        protected override string GetKeyForItem(Item item)
+        {
+            return item.Name;
+        }
+    }
+}
