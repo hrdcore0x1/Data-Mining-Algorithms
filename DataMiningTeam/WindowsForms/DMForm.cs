61ab7fcd46794f76b1b1e3edb953742430e784f6
Cory Nance <corysemail@gmail.com>
2012-10-24 08:50:28 -0400

    merge probs!!

--- DataMiningTeam/WindowsForms/DMForm.cs
+++ DataMiningTeam/WindowsForms/DMForm.cs
@@ -1,3 +1,4 @@
+<<<<<<< origin/master:DataMiningTeam/WindowsForms/DMForm.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,3 +18,24 @@ namespace DataMiningTeam.WindowsForms
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataMiningTeam.WindowsForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
+>>>>>>> local:DataMiningTeam/WindowsForms/MainForm.cs
